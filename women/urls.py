63d496a492dfb21203d2046472de1455fceacd02
hailMeh <Sultanhabibi@mail.ru>
2022-04-08 13:36:00 +0300

    initial

--- women/urls.py
+++ women/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
-from .views import index, category
+from django.urls import path, re_path
+from .views import index, categories, archive, pageNotFound
 
 urlpatterns = [
     path('', index, name='index'),
-    path('cats/', category, name='category'),
+    path('cats/<slug:cat>', categories, name='category'),  # slug = text/int
+    re_path(r'^archive/(?P<year>[0-9]{4})/', archive, name='archive')
+    # регулярки [0-9] - разрешенные цифры , {4} - разрешенное кол-во цифр
 ]
+
+handler404 = pageNotFound  # Обработчик Хэндлеров, дебюг в false
--- women/views.py
+++ women/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 
 def index(request):
     return HttpResponse('<h1>INDEX</h1>')
 
 
-def category(request):
-    return HttpResponse('<h1>CATEGORY</h1>')
+def categories(request, cat):
+    '''   if int(cat) > 2020:
+       return redirect('index')  # Проверяемый параметр должен совпадать с параметром у URL !!!  '''
+    return HttpResponse(f"<h1>Статьи по категориям</h1>{cat}</p>")
 
+
+def archive(request, year):
+    if int(year) > 2022:  # Если год больше нынешнего, дай ошибку,которая перенаправит на хэндлер404
+        raise Http404()  #  return redirect('/')
+    return HttpResponse(f'ARCHIVE FOR {year} year')
+
+
+
+def pageNotFound(request, exception):  # Страница не найдена
+    return HttpResponseNotFound('NOT FOUND PAGE')
 
