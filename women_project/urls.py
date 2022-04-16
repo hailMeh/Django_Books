a4f02d164e94606247e298d22ddab83a8cf49102
hailMeh <72820301+hailMeh@users.noreply.github.com>
2022-04-16 11:31:36 +0300

    Update urls.py

--- women_project/urls.py
+++ women_project/urls.py
@@ -21,3 +21,25 @@ if settings.DEBUG:
                       path('__debug__/', include(debug_toolbar.urls)),
                   ] + urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Чтобы из статики картинки добавлялись в юрл для показа
+
+''' для разворачивания в инете, статика чтобы загружалась. импорт двух библиотек а дальше копипаста в if  
+    from django.views.static import serve as mediaserve
+from django.conf.urls import url
+ 
+if settings.DEBUG:
+    import debug_toolbar
+ 
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
+ 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
+else:
+    urlpatterns += [
+        url(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+            mediaserve, {'document_root': settings.MEDIA_ROOT}),
+        url(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
+            mediaserve, {'document_root': settings.STATIC_ROOT}),
+    ]
+  '''
